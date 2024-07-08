c45f486e395e115d901bbaa7733ece3abfca607d
Suyash Labde <suyashlabde123@gmail.com>
2024-07-08 11:29:21 +0530

    updated navbar ui

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,64 +1,89 @@
-import React from 'react';
-import { FaHome, FaNetworkWired, FaBriefcase, FaEnvelope, FaBell, FaAngleDown, FaTh } from 'react-icons/fa';
+'use client'
+import React, { useState } from 'react';
+import { FaHome, FaNetworkWired, FaBriefcase, FaEnvelope, FaBell, FaAngleDown, FaTh, FaBars, FaTimes } from 'react-icons/fa';
 import { FiSearch } from 'react-icons/fi';
-import { TbCloudDataConnection } from "react-icons/tb";
-import { FaLinkedin } from "react-icons/fa6";
+import { FaLinkedin } from 'react-icons/fa6';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className='bg-white'>
-      <div className='md:mx-8 py-3 flex flex-row w-full'>
-        <div className='grid grid-cols-2 w-[20%] ml-40'>
-        <TbCloudDataConnection  className="w-9 h-9 text-gray-500 dark:text-black-900" />
-          <div className="relative">
+    <div className='bg-white px-40'>
+      <div className='flex justify-between items-center py-3 px-4 md:px-8'>
+        <div className='flex items-center'>
+          <FaLinkedin className="w-9 h-9 text-blue-600" />
+          <div className="relative ml-4">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <FiSearch className="w-4 h-4 text-gray-100 dark:text-gray-100" />
+              <FiSearch className="w-4 h-4 text-gray-500" />
             </div>
-            <input type="search" id="search" className="block w-100% p-2.5 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-blue-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
-          </div>
+            <input type="search" id="search" className="block w-100px p-2.5 pl-20 text-sm text-gray-900 border border-gray-300 rounded-lg bg-transparent focus:ring-blue-500 focus:border-blue-500"placeholder="Search" />
+          </div>  
         </div>
-        <div className='mx-28 flex flex-row'>
-          <div className='grid grid-cols-7 px-22'>
-            <div className='flex flex-col items-center'>
-              <FaHome className='h-6 w-6' />
-              <small className='font-SourceSansProLight text-sm'>Home</small>
-            </div>
-            <div className='flex flex-col items-center'>
-              <FaNetworkWired className='h-6 w-6' />
-              <small className="font-SourceSansProLight text-sm">My Network</small>
-            </div>
-            <div className='flex flex-col items-center'>
-              <FaBriefcase className='h-6 w-6' />
-              <small className="font-SourceSansProLight text-sm">Jobs</small>
-            </div>
-            <div className='flex flex-col items-center'>
-              <FaEnvelope className='h-6 w-6' />
-              <small className="font-SourceSansProLight text-sm">Messaging</small>
-            </div>
-            <div className='flex flex-col items-center'>
-              <FaBell className='h-6 w-6' />
-              <small className="font-SourceSansProLight text-sm">Notifications</small>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" className='w-6 h-6 rounded-full' alt="profile" />
-              <div className='flex flex-row'>
-                <small className="font-SourceSansProLight text-sm">Me </small>
-                <FaAngleDown className='h-4 w-4 mt-1' />
-              </div>
-            </div>
-            <div className='flex flex-col items-center'>
-              <FaTh className='w-6 h-6' />
-              <div className='flex flex-row'>
-                <small className="font-SourceSansProLight text-sm">For Business</small>
-                <FaAngleDown className='h-4 w-4 mt-1' />
-              </div>
-            </div>
-          </div>
-          <div className='w-28 mx-4 flex items-center justify-center'>
-            <small className="font-SourceSansProRegular text-sm underline text-wrap text-yellow-700">Network Smarter, Try Premium Free</small>
-          </div>
+        <div className='md:hidden'>
+          <button onClick={toggleMenu}>
+            {menuOpen ? <FaTimes className="h-6 w-6" /> : <FaBars className="h-6 w-6" />}
+          </button>
+        </div>
+        <div className={`hidden md:flex space-x-8`}>
+          <a href="#" className='flex flex-col items-center'>
+            <FaHome className='h-6 w-6' />
+            <small className='font-SourceSansProLight text-sm'>Home</small>
+          </a>
+          <a href="#" className='flex flex-col items-center'>
+            <FaNetworkWired className='h-6 w-6' />
+            <small className="font-SourceSansProLight text-sm">My Network</small>
+          </a>
+          <a href="#" className='flex flex-col items-center'>
+            <FaBriefcase className='h-6 w-6' />
+            <small className="font-SourceSansProLight text-sm">Jobs</small>
+          </a>
+          <a href="#" className='flex flex-col items-center'>
+            <FaEnvelope className='h-6 w-6' />
+            <small className="font-SourceSansProLight text-sm">Messaging</small>
+          </a>
+          <a href="#" className='flex flex-col items-center'>
+            <FaBell className='h-6 w-6' />
+            <small className="font-SourceSansProLight text-sm">Notifications</small>
+          </a>
         </div>
       </div>
+      {menuOpen && (
+        <div className='md:hidden fixed inset-0 bg-gray-800 bg-opacity-75 z-50'>
+          <div className="bg-white w-64 h-full p-4">
+            <div className="flex justify-end mb-4">
+              <button onClick={toggleMenu}>
+                <FaTimes className="h-6 w-6" />
+              </button>
+            </div>
+            <div className='flex flex-col space-y-4'>
+              <a href="#" className='flex items-center'>
+                <FaHome className='h-6 w-6 mr-2' />
+                <small className='font-SourceSansProLight text-sm'>Home</small>
+              </a>
+              <a href="#" className='flex items-center'>
+                <FaNetworkWired className='h-6 w-6 mr-2' />
+                <small className="font-SourceSansProLight text-sm">My Network</small>
+              </a>
+              <a href="#" className='flex items-center'>
+                <FaBriefcase className='h-6 w-6 mr-2' />
+                <small className="font-SourceSansProLight text-sm">Jobs</small>
+              </a>
+              <a href="#" className='flex items-center'>
+                <FaEnvelope className='h-6 w-6 mr-2' />
+                <small className="font-SourceSansProLight text-sm">Messaging</small>
+              </a>
+              <a href="#" className='flex items-center'>
+                <FaBell className='h-6 w-6 mr-2' />
+                <small className="font-SourceSansProLight text-sm">Notifications</small>
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
